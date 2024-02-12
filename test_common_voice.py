74893a1c62a6a200d2cdfef7d34f8c439e1a6fdb
Marc Altmann <marcaltmann@posteo.de>
2024-02-12 08:36:43 +0100

    Print length of datasets

--- test_common_voice.py
+++ test_common_voice.py
@@ -1,6 +1,10 @@
+import random
 from datasets import load_dataset
 
 cv_16 = load_dataset('mozilla-foundation/common_voice_16_1', 'de',
                      split='train', trust_remote_code=True)
-first = next(iter(cv_16))
-print(first)
+length = len(cv_16)
+example = random.choice(cv_16)
+
+print(f"{length} datapoints")
+print(f"Example: {example}")
--- test_fleurs.py
+++ test_fleurs.py
@@ -1,6 +1,10 @@
+import random
 from datasets import load_dataset
 
 fleurs = load_dataset('google/fleurs', 'de_de', split='train',
                       trust_remote_code=True)
-first = next(iter(fleurs))
-print(first)
+length = len(fleurs)
+example = random.choice(fleurs)
+
+print(f"{length} datapoints")
+print(f"Example: {example}")
